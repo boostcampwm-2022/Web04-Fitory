e80434c1877a2a66e3c00c080e33f081467add48
KingDonggyu <dgkim69166916@gmail.com>
2022-11-16 09:27:54 +0900

    ✨ [feat] #30 : 뒤로가기 기능 구현

--- client/src/components/BackButton/index.tsx
+++ client/src/components/BackButton/index.tsx
@@ -3,8 +3,12 @@ import arrowLeftSrc from "@public/icons/btn_arrow_left.svg";
 import * as s from "./style";
 
 const BackButton = () => {
+  const handleOnClick = () => {
+    window.history.back();
+  };
+
   return (
-    <button type="button">
+    <button type="button" onClick={handleOnClick}>
       <s.Image src={arrowLeftSrc} alt="뒤로가기 버튼" />
     </button>
   );
