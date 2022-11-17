ad003cf65aa359e438f8557e9b20ce68a34f45a0
Choi siun <siun0331@gmail.com>
2022-11-17 18:06:40 +0900

    ✨ [feat]#52: 프로필 사진 컨테이너 이미지 업로드 기능 구현

--- client/src/components/ProfileImageContainer/index.tsx
+++ client/src/components/ProfileImageContainer/index.tsx
@@ -1,14 +1,37 @@
+import React, { LegacyRef, useRef, useState } from "react";
+import defaultProfileImage from "@public/images/img_default_profile.svg";
 import * as s from "./style";
 
-export interface ProfileImageContainerProps{
-  isProfile: boolean;
-}
-const ProfileImageContainer = ({isProfile}: ProfileImageContainerProps) => {
-  return(
+const ProfileImageContainer = () => {
+  const [image, setImage] = useState<string>(defaultProfileImage);
+  const imgRef = useRef<HTMLInputElement>(null);
+  const onChange = () => {
+    const fileReader = new FileReader();
+    const file = imgRef.current?.files as FileList;
+
+    fileReader.readAsDataURL(file[0]);
+    fileReader.onload = () => {
+      setImage(fileReader.result as string);
+    };
+  };
+  return (
     <s.Wrapper>
-      <h1>hi</h1>
+      <s.ImageContainer
+        src={image}
+        onClick={() => {
+          imgRef.current?.click();
+        }}
+      />
+      <input
+        type="file"
+        style={{ display: "none" }}
+        accept="image/jpg, image/png, image/jpeg"
+        name="profile_img"
+        onChange={onChange}
+        ref={imgRef as LegacyRef<HTMLInputElement>}
+      />
     </s.Wrapper>
   );
-}
+};
 
-export default ProfileImageContainer
+export default ProfileImageContainer;
--- client/src/components/ProfileImageContainer/style.ts
+++ client/src/components/ProfileImageContainer/style.ts
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  width: 25%;
+  width: 100%;
   height: 100%;
-`
+`;
 
-export const ImageContainer = styled.image`
-  
-`
+export const ImageContainer = styled.img`
+  width: 100%;
+
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.COLORS.LIGHT_BLUE};
+`;
