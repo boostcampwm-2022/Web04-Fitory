390147082e1dbf34a9b8ced6546a3749ec381c00
Choi siun <siun0331@gmail.com>
2022-11-17 20:30:23 +0900

    🧪 [test]#52: 페이지(props)에 따라 input 활성화 테스트 작성

--- client/src/stories/ProfileImageContainer.stories.tsx
+++ client/src/stories/ProfileImageContainer.stories.tsx
@@ -1,10 +1,22 @@
-import { Meta } from "@storybook/react";
 import ProfileImageContainer from "@components/ProfileImageContainer";
 import React from "react";
+import { Story } from "@storybook/react";
 
 export default {
   title: "components/ProfileImageContainer",
   component: ProfileImageContainer,
-} as Meta;
+};
 
-export const defaultProfileImageContainer = () => <ProfileImageContainer />;
+const Template: Story<{ isModified: boolean }> = (args: { isModified: boolean }) => (
+  <ProfileImageContainer {...args} />
+);
+
+export const DefaultProfileImageContainer = Template.bind({});
+DefaultProfileImageContainer.args = {
+  isModified: false,
+};
+
+export const EditProfileImageContainer = Template.bind({});
+DefaultProfileImageContainer.args = {
+  isModified: true,
+};
