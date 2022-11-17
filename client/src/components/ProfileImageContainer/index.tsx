fb001a84dccd8978103952e9c68422fba1789daa
Choi siun <siun0331@gmail.com>
2022-11-17 13:33:03 +0900

    📸 [resource]#52: 프로필 기본 이미지 추가

---
+++ client/src/components/ProfileImageContainer/index.tsx
@@ -0,0 +1,14 @@
+import * as s from "./style";
+
+export interface ProfileImageContainerProps{
+  isProfile: boolean;
+}
+const ProfileImageContainer = ({isProfile}: ProfileImageContainerProps) => {
+  return(
+    <s.Wrapper>
+      <h1>hi</h1>
+    </s.Wrapper>
+  );
+}
+
+export default ProfileImageContainer
---
+++ client/src/components/ProfileImageContainer/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 25%;
+  height: 100%;
+`
+
+export const ImageContainer = styled.image`
+  
+`
