91576d7f9bd49a9efebd945df59423f310d68eae
KingDonggyu <dgkim69166916@gmail.com>
2022-11-19 18:57:37 +0900

    ✨ [feat] #51 : 잔디 캘린더 배열 반환 유틸 함수 구현

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -18,7 +18,7 @@ module.exports = merge(commonConfig, {
   // 빌드 결과물은 실제 파일로 빌드되진 않고 메모리에 저장된다.
   devServer: {
     static: path.resolve(__dirname, "dist"),
-    port: 3002,
+    port: 3000,
     open: true,
     hot: true, // 빌드 대상 파일이 변경되면 자동으로 브라우저를 새로고침
     historyApiFallback: true, // history api route에 대한 fallback
