e17e2cb987457e50cbd20ace32072a02448556d6
essential2189 <70889358+essential2189@users.noreply.github.com>
2022-11-22 02:00:12 +0900

    ✨ [feat] #95 : 스웨거 작성

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Query } from "@nestjs/common";
+import { ApiOperation, ApiQuery } from "@nestjs/swagger";
 import { UsersService } from "./users.service";
 
 @Controller("users")
@@ -6,6 +7,13 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get("get")
+  @ApiOperation({
+    summary: "해당 사용자의 모든 정보를 반환",
+  })
+  @ApiQuery({
+    name: "id",
+    type: "number",
+  })
   async getUserInfo(@Query("id") id: number) {
     return this.usersService.getUserInfo(id);
   }
