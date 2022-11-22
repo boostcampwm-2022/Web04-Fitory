211985691ca5f781ed8855a301f9db2ce9a02418
edac99 <jamjam1208@naver.com>
2022-11-22 15:50:40 +0900

    ♻️ [refactor] #94 : Find History Of Month API 수정
    특정 사용자의 특정 월의 운동 기록을 반환하는 API를 queryBuilder 문법으로 수정

--- server/src/exercises/exercises.service.ts
+++ server/src/exercises/exercises.service.ts
@@ -2,7 +2,7 @@ import { HistoryOfMonthDto } from "./dto/history-of-month.dto";
 import { Exercise } from "./entities/exercise.entity";
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Equal, Like, Repository } from "typeorm";
+import { Repository } from "typeorm";
 import { EveryDateDto } from "./dto/every-date.dto";
 
 @Injectable()
@@ -22,10 +22,11 @@ export class ExercisesService {
   }
 
   async findExerciseHistoryOfMonth(month: number, userId: number) {
-    const exerciseHistory = await this.exerciseRepository.findBy({
-      user: { id: Equal(userId) },
-      date: Like(`__${month.toString().padStart(2, "0")}__`),
-    });
+    const exerciseHistory = await this.exerciseRepository
+      .createQueryBuilder("exercise")
+      .where("exercise.user_id = :userId", { userId })
+      .andWhere("exercise.date like :date", { date: `__${month.toString().padStart(2, "0")}__` })
+      .getMany();
     return new HistoryOfMonthDto(exerciseHistory);
   }
 }
