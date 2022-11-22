ca0d56e0de56d1d889428ef67b13b353a74a4803
KingDonggyu <dgkim69166916@gmail.com>
2022-11-22 18:55:17 +0900

    ✨ [feat] #115 : 3대 챌린지 버튼, 운동 기록 버튼 디자인

--- client/src/pages/HomePage/index.tsx
+++ client/src/pages/HomePage/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import PageTemplate from "@pages/PageTemplate";
 import UserInfoSummary from "@components/UserInfoSummary";
 import CalendarHeatMap from "@components/CalendarHeatMap";
+import arrowRightSrc from "@public/icons/btn_arrow_right.svg";
 import * as s from "./style";
 
 const HomePage = () => {
@@ -9,10 +10,13 @@ const HomePage = () => {
     <PageTemplate isRoot>
       <s.Wrapper>
         <UserInfoSummary />
-        {/* 3대 챌린지 도전하기 버튼 */}
+        <s.ChallengeButton type="button">
+          <h3>3대 챌린지 도전하기</h3>
+          <img src={arrowRightSrc} alt="3대 챌린지 도전하기 버튼" />
+        </s.ChallengeButton>
         <CalendarHeatMap />
         {/* 운동 기록 캘린더 */}
-        {/* 운동 기록하기 버튼 */}
+        <s.RecordButton type="button">운동 기록하기</s.RecordButton>
       </s.Wrapper>
     </PageTemplate>
   );
