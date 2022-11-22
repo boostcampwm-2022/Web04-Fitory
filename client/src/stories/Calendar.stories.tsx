8fdb97df16c3c55902bd06439280d51f26e01aa3
Choi siun <siun0331@gmail.com>
2022-11-22 17:30:42 +0900

    🧪 [test]#76: 캘린더 스토리북  코드 경로 수정

--- client/src/stories/Calendar.stories.tsx
+++ client/src/stories/Calendar.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Meta } from "@storybook/react";
-import Calendar from "../components/Calendar";
+import Calendar from "@components/Calendar";
 
 export default {
   title: "components/Calendar",
