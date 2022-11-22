680239e6d6015eef4c03f6be13d6dd971104d4b9
essential2189 <70889358+essential2189@users.noreply.github.com>
2022-11-22 19:08:12 +0900

    ✨ [feat] #66 : user info dto 작성

---
+++ server/src/users/dto/users-info.dto.ts
@@ -0,0 +1,25 @@
+export class UsersInfoDto {
+  profile_image: string;
+
+  oauthId: string;
+
+  name: string;
+
+  age: number;
+
+  gender: number;
+
+  height: number;
+
+  weight: number;
+
+  introduce: string;
+
+  tier: number;
+
+  follower_count: number;
+
+  following_count: number;
+
+  volume_sum: number;
+}
