5c9adf9414a298bcf945613534f4be71b78dbe9a
essential2189 <70889358+essential2189@users.noreply.github.com>
2022-11-22 10:31:18 +0900

    ✨ [feat] #94 : 스웨거 @ApiOperation 추가

--- server/src/oauth/google-oauth/google-oauth.controller.ts
+++ server/src/oauth/google-oauth/google-oauth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Inject, Req, Res, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { Response } from "express";
+import { ApiOperation } from "@nestjs/swagger";
 import { GoogleOauthService } from "./google-oauth.service";
 import { RequestWithUser } from "../../types/request";
 
@@ -10,6 +11,9 @@ export class GoogleOauthController {
 
   @Get()
   @UseGuards(AuthGuard("google"))
+  @ApiOperation({
+    summary: "구글 로그인 페이지로 리다이렉트",
+  })
   googleOAuth() {
     // redirect google login page
     return { msg: "Google Authentication" };
@@ -17,6 +21,9 @@ export class GoogleOauthController {
 
   @Get("callback")
   @UseGuards(AuthGuard("google"))
+  @ApiOperation({
+    summary: "구글 로그인 callback, 쿠키 생성 후 사용자에게 전송",
+  })
   async googleOAuthCallback(@Req() req: RequestWithUser, @Res() res: Response) {
     const token = await this.googleOauthService.signIn(req.user);
 
