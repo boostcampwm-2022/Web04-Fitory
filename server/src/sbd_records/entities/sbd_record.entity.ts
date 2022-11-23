c366e4ea7e6658832be1f3994106a925429f8c37
edac99 <79911816+edac99@users.noreply.github.com>
2022-11-23 18:07:03 +0900

    ♻️ [refactor] #131 : ERD 클라우드 기준 맞춰서 Entity 수정
    nullable: false 추가 및 네이밍 변경

--- server/src/sbd_records/entities/sbd_record.entity.ts
+++ server/src/sbd_records/entities/sbd_record.entity.ts
@@ -21,14 +21,14 @@ export class SBD_record {
   @Column({ name: "date", length: 45 })
   date!: string;
 
-  @Column({ name: "time_stamp", type: "bigint" })
-  timeStamp!: string;
+  @Column({ name: "second_stamp" })
+  secondStamp!: number;
 
   @Column({ name: "user_weight" })
   userWeight!: number;
 
   // FK
-  @ManyToOne(() => User)
+  @ManyToOne(() => User, { nullable: false })
   @JoinColumn({ name: "user_id" })
   user!: User;
 }
