ba6bc4a14ab8978a9f6ab30822c7935a8cdad1ba
essential2189 <70889358+essential2189@users.noreply.github.com>
2022-11-23 20:19:02 +0900

    ✨ [feat] #141 : DTO 설정 추가

--- server/src/oauth/google-oauth/dto/google-user.dto.ts
+++ server/src/oauth/google-oauth/dto/google-user.dto.ts
@@ -1,5 +1,19 @@
-export class GoogleUserDto {
-  readonly oauthId: string;
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+import { isBigInt } from "@nestjs/swagger/dist/plugin/utils/ast-utils";
 
-  readonly name: string;
+export class GoogleUserDto {
+  @ApiProperty({
+    description: "유저의 고유 OAuth ID",
+    type: isBigInt,
+  })
+  @IsString()
+  oauthId: string;
+
+  @ApiProperty({
+    description: "유저의 이름",
+    type: String,
+  })
+  @IsString()
+  name: string;
 }
