ff2e58bb4d5d97ee793af4df0b430cb4cf8309f5
KingDonggyu <dgkim69166916@gmail.com>
2022-11-23 17:21:17 +0900

    💄 [style] #135 : 네비게이션바 fixed 설정 및 Template 컴포넌트 레이아웃 배치 수정

--- client/src/components/MainContainer/style.ts
+++ client/src/components/MainContainer/style.ts
@@ -2,12 +2,14 @@ import styled from "styled-components";
 
 export const Background = styled.div`
   width: 100%;
-  height: 100%;
+  height: fit-content;
+  /* flex-grow: 1; */
+  /* overflow-y: auto; */
 `;
 
 export const Wrapper = styled.div`
   margin: auto;
-  padding: 20px;
+  padding: ${({ theme }) => theme.NAVBAR_HEIGHT + 20}px 20px;
   height: 100%;
   max-width: ${({ theme }) => theme.MAX_WIDTH.DEFAULT};
   & > * {
