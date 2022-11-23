935470f9747d08a781dd968ad0fbacedddb1781f
KingDonggyu <dgkim69166916@gmail.com>
2022-11-23 16:56:16 +0900

    ✨ [feat] #126 : Google OAuth 로그인 클라이언트 연동

--- client/src/components/GoogleOAuthButton/index.tsx
+++ client/src/components/GoogleOAuthButton/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import GoogleLogin from "react-google-login";
 import googleSignInSrc from "@public/images/btn_google_signin.png";
 import * as s from "./style";
 
-const GoogleOAuthButton = () => {
+const GoogleCustomButton = ({ onClick }: { onClick: () => void }) => {
   return (
-    <s.Button type="button">
+    <s.Button type="button" onClick={onClick}>
       <img src={googleSignInSrc} alt="구글 로그인 버튼" />
     </s.Button>
+  );
+};
+
+const GoogleOAuthButton = () => {
+  return (
+    <GoogleLogin
+      clientId={process.env.GOOGLE_CLIENT_ID as string}
+      render={({ onClick }) => <GoogleCustomButton onClick={onClick} />}
+    />
   );
 };
 
