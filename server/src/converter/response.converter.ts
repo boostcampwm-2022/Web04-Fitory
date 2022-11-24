663eb3e74842cf6fc8942c3ed18b2de697a58928
edac99 <79911816+edac99@users.noreply.github.com>
2022-11-24 16:48:54 +0900

    ✨ [feat] #139 : API 응답 단일화 메서드 구현
    HttpResponse.success로 단일화

---
+++ server/src/converter/response.converter.ts
@@ -0,0 +1,19 @@
+import { HttpStatus } from "@nestjs/common";
+
+export const HttpResponse = {
+  success: (response: unknown) => {
+    return {
+      ok: true,
+      statusCode: HttpStatus.OK,
+      response,
+    };
+  },
+
+  failed: (statusCode: number, message: string) => {
+    return {
+      ok: false,
+      statusCode: statusCode,
+      response: message,
+    };
+  },
+};
--- server/src/domain/exercises/exercises.service.ts
+++ server/src/domain/exercises/exercises.service.ts
@@ -1,8 +1,10 @@
+import { HttpResponse } from "@converter/response.converter";
 import { exerciseConverter } from "./converter/exercise.converter";
 import { Exercise } from "./entities/exercise.entity";
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { Exception } from "src/exception/exceptions";
 
 @Injectable()
 export class ExercisesService {
@@ -18,12 +20,9 @@ export class ExercisesService {
       .where("exercise.user_id = :userId", { userId })
       .orderBy("CAST (exercise.date AS SIGNED)", "ASC")
       .getMany();
-    return {
-      ok: true,
-      response: {
-        dateList: exerciseConverter.dateList(exerciseRows),
-      },
-    };
+    return HttpResponse.success({
+      dateList: exerciseConverter.dateList(exerciseRows),
+    });
   }
 
   async findExerciseHistoryOfMonth(month: number, userId: number) {
@@ -32,24 +31,18 @@ export class ExercisesService {
       .where("exercise.user_id = :userId", { userId })
       .andWhere("exercise.date like :date", { date: `__${month.toString().padStart(2, "0")}__` })
       .getMany();
-    return {
-      ok: true,
-      response: {
-        dateList: exerciseConverter.historyOfMonth(exerciseHistory),
-      },
-    };
+    return HttpResponse.success({
+      historyList: exerciseConverter.historyOfMonth(exerciseHistory),
+    });
   }
 
   async getProfileData(userId: number) {
     const totalVolume = await this.getTotalVolume(userId);
     const totalExerciseDate = await this.getTotalExerciseDate(userId);
-    return {
-      ok: true,
-      response: {
-        totalVolume,
-        totalExerciseDate,
-      },
-    };
+    return HttpResponse.success({
+      totalVolume,
+      totalExerciseDate,
+    });
   }
 
   async getTotalVolume(userId: number) {
