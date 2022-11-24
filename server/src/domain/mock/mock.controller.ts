7684018658124822420ed241410b391268e45c9c
essential2189 <70889358+essential2189@users.noreply.github.com>
2022-11-24 23:23:55 +0900

    ♻️ [refactor] #169: mock 생성기 리팩토링

--- server/src/domain/mock/mock.controller.ts
+++ server/src/domain/mock/mock.controller.ts
@@ -16,28 +16,28 @@ export class MockController {
   })
   async mockDataGenerator(@Query("num") num: number) {
     // 실행 순서 중요함
-    const userNums = Array.from({ length: num }, (_, i) => i + 1); // 유저 수
-    const followNums = Array.from(Array(num * 2).keys()); // 무작위 num*2 만큼의 팔로우<->팔로잉 관계 생성
-    await Promise.all(
-      followNums.map(() => {
-        return this.mockService.mockFollow(userNums);
-      }),
-    );
-
-    await this.mockService.mockUsers(userNums);
-
-    const recordNums = Array.from(Array(12).keys()); // 각 유저가 1년 이내의 무작위 날짜로 12번만큼 챌린지 기록
-    await Promise.all(
-      recordNums.map(() => {
-        return this.mockService.mockRecord();
-      }),
-    );
+    // const userNums = Array.from({ length: num }, (_, i) => i + 1); // 유저 수
+    // const followNums = Array.from(Array(num * 2).keys()); // 무작위 num*2 만큼의 팔로우<->팔로잉 관계 생성
+    // await Promise.all(
+    //   followNums.map(() => {
+    //     return this.mockService.mockFollow(userNums);
+    //   }),
+    // );
+    //
+    // await this.mockService.mockUsers(userNums);
+    //
+    // const recordNums = Array.from(Array(12).keys()); // 각 유저가 1년 이내의 무작위 날짜로 12번만큼 챌린지 기록
+    // await Promise.all(
+    //   recordNums.map(() => {
+    //     return this.mockService.mockRecord();
+    //   }),
+    // );
 
     await this.mockService.mockStatistics();
 
     await this.mockService.mockRoutine();
 
-    const exerciseNum = Array.from(Array(60).keys()); // 각 유저가 최근 3개월 이내의 무작위 날짜로 3세트 짜리 운동을 60개 기록
+    const exerciseNum = Array.from(Array(100).keys()); // 각 유저가 최근 6개월 이내의 무작위 날짜로 3세트 짜리 운동을 100개 기록
     await Promise.all(
       exerciseNum.map(() => {
         return this.mockService.mockExercise();
