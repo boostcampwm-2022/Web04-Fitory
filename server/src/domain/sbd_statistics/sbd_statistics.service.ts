a0ad0686b80c9451acc49a1f30e015ecf6324893
edac99 <79911816+edac99@users.noreply.github.com>
2022-11-24 15:42:57 +0900

    ✨ [feat] #134 : Statistics API 세팅
    Dto는 Service 구현 이후 담당자가 세팅하도록 함

--- server/src/domain/sbd_statistics/sbd_statistics.service.ts
+++ server/src/domain/sbd_statistics/sbd_statistics.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { SBD_statistics } from "./entities/sbd_statistics.entity";
 
 @Injectable()
-export class SbdStatisticsService {}
+export class SbdStatisticsService {
+  constructor(
+    @InjectRepository(SBD_statistics)
+    private exerciseRepository: Repository<SBD_statistics>,
+  ) {}
+
+  getEverySBDData() {
+    return {
+      ok: true,
+      response: {},
+    };
+  }
+}
