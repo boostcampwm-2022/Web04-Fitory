58b86cbc4172d3b2319373dfa4fc7b6b924ac3d2
essential2189 <70889358+essential2189@users.noreply.github.com>
2022-11-24 18:56:14 +0900

    ✨ [feat] #145 : mock 데이터 생성기 구현

--- server/src/mock/mock.controller.ts
+++ server/src/mock/mock.controller.ts
@@ -8,17 +8,46 @@ export class MockController {
 
   @Get()
   @ApiOperation({
-    summary: "mock 데이터 생성",
+    summary: "mock 데이터 생성 ❗️배포시 반드시 삭제",
   })
   @ApiQuery({
     name: "num",
     type: "number",
   })
   async mockDataGenerator(@Query("num") num: number) {
-    const data = await this.mockService.getOpenData(num);
-
-    await this.mockService.mockUsers(data);
+    const userNums = Array.from({ length: num }, (_, i) => i + 1); // 유저 수
+    await this.mockService.mockUsers(userNums);
     console.log("mock users");
+
+    const recordNums = Array.from(Array(12).keys()); // 각 유저가 1년 이내의 무작위 날짜로 12번만큼 챌린지 기록
+    await Promise.all(
+      recordNums.map(() => {
+        return this.mockService.mockRecord();
+      }),
+    );
+    console.log("mock record");
+
+    // await this.mockService.mockStatistics();
+    // console.log("mock statistics");
+
+    await this.mockService.mockRoutine();
+    console.log("mock routine");
+
+    const exerciseNum = Array.from(Array(200).keys()); // 각 유저가 1년 이내의 무작위 날짜로 3세트 짜리 운동을 200개 기록
+    await Promise.all(
+      exerciseNum.map(() => {
+        return this.mockService.mockExercise();
+      }),
+    );
+    console.log("mock exercise");
+
+    const alarmNum = Array.from(Array(5).keys()); // 각 유저가 5개의 알림을 받음
+    await Promise.all(
+      alarmNum.map(() => {
+        return this.mockService.mockAlarm();
+      }),
+    );
+    console.log("mock alarm");
 
     return "mock data complete";
   }
--- server/src/mock/mock.service.ts
+++ server/src/mock/mock.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from "@nestjs/common";
-import path from "path";
 import fs from "fs";
 import { faker } from "@faker-js/faker";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
-import { DataType } from "../types/openData";
+import dayjs from "dayjs";
+import { OpenDataType } from "../types/openDataStatistics";
 import { User } from "../users/entities/user.entity";
 import { SBD_record } from "../sbd_records/entities/sbd_record.entity";
+import { SBD_statistics } from "../sbd_statistics/entities/sbd_statistics.entity";
+import { Routine } from "../routines/entities/routine.entity";
+import { Follow } from "../follows/entities/follow.entity";
+import { Exercise } from "../exercises/entities/exercise.entity";
+import { Alarm } from "../alarms/entities/alram.entity";
 
 faker.locale = "ko";
 
@@ -17,40 +22,42 @@ export class MockService {
     private userRepository: Repository<User>,
     @InjectRepository(SBD_record)
     private recordsRepository: Repository<SBD_record>,
+    @InjectRepository(SBD_statistics)
+    private statisticsRepository: Repository<SBD_statistics>,
+    @InjectRepository(Routine)
+    private routineRepository: Repository<Routine>,
+    @InjectRepository(Follow)
+    private followRepository: Repository<Follow>,
+    @InjectRepository(Exercise)
+    private exerciseRepository: Repository<Exercise>,
+    @InjectRepository(Alarm)
+    private alarmRepository: Repository<Alarm>,
   ) {}
 
-  async getOpenData(n: number) {
-    const data: DataType[] = [];
+  async getOpenData(file: string) {
+    const data: OpenDataType[] = [];
 
-    const csvFile = fs.readFileSync("src/mock/open-powerlifting-fillna.csv", "utf8");
-    const csvSplit = csvFile.toString().split("\n");
+    let fileName;
+    if (file === "fillna") fileName = "src/mock/open-powerlifting-fillna.csv";
+    else if (file === "sampling") fileName = "src/mock/open-powerlifting-sampling.csv";
+    const csvFile = fs.readFileSync(fileName, "utf8");
+    const csvSplits = csvFile.toString().split("\n");
 
-    for (let i = 0; i < n; i += 1) {
-      const csvData = csvSplit[i].split(",");
-      console.log(csvData);
-      const name = faker.name.lastName() + faker.name.firstName();
+    for (let i = 0; i < csvSplits.length; i += 1) {
+      const csvData = csvSplits[i].split(",");
 
       let gender;
       if (csvData[0] === "M") gender = 0;
       else if (csvData[0] === "F") gender = 1;
 
       const sbd = csvData[1];
-      const age = Math.floor(+csvData[2]);
       const weight = Math.floor(+csvData[3]);
-      const squat = Math.floor(+csvData[5]);
-      const bench = Math.floor(+csvData[6]);
-      const deadlift = Math.floor(+csvData[7]);
-      const total = Math.floor(+csvData[8]);
+      const total = Math.floor(+csvData[8] / 2);
 
-      if (sbd === "SBD" && age && weight && squat && bench && deadlift && total) {
+      if (sbd === "SBD" && weight && total) {
         data.push({
-          name,
           gender,
-          age,
           weight,
-          squat,
-          bench,
-          deadlift,
           total,
         });
       }
@@ -58,26 +65,181 @@ export class MockService {
     return data;
   }
 
-  async mockUsers(dataList: DataType[]) {
+  async mockUsers(num: number[]) {
     return Promise.all(
-      dataList.map((data: DataType) => {
-        console.log(data);
+      num.map(() => {
         return this.userRepository
-          .createQueryBuilder("user")
+          .createQueryBuilder()
           .insert()
           .into(User)
           .values({
-            name: data.name,
-            age: data.age,
-            gender: data.gender,
-            height: data.weight + 100,
-            weight: data.weight,
+            name: faker.name.lastName() + faker.name.firstName(),
+            age: faker.datatype.number({ min: 10, max: 60 }),
+            gender: faker.datatype.number({ min: 0, max: 1 }),
+            height: faker.datatype.number({ min: 150, max: 200 }),
+            weight: faker.datatype.number({ min: 50, max: 150 }),
             introduce: faker.lorem.sentence(),
             volumeSum: faker.datatype.number({ min: 0, max: 100000 }),
             tier: faker.datatype.number({ min: 0, max: 6 }),
             followerCount: faker.datatype.number({ min: 0, max: 1000 }),
             followingCount: faker.datatype.number({ min: 0, max: 1000 }),
             oauthId: faker.datatype.number({ min: 0, max: 999999999 }).toString(),
+            profileImage: "http://default.image",
+          })
+          .execute();
+      }),
+    );
+  }
+
+  async mockRecord() {
+    const allUserId = await this.userRepository.createQueryBuilder().select("user_id").getRawMany();
+
+    return Promise.all(
+      allUserId.map(async (id: { user_id: number }) => {
+        const weight = await this.userRepository
+          .createQueryBuilder()
+          .where("user_id = :n", { n: id })
+          .select("weight")
+          .getOne();
+        const squat = faker.datatype.number({ min: 20, max: 300 });
+        const deadlift = faker.datatype.number({ min: 20, max: 300 });
+        const benchpress = faker.datatype.number({ min: 20, max: 300 });
+        return this.recordsRepository
+          .createQueryBuilder()
+          .insert()
+          .into(SBD_record)
+          .values({
+            user: { id: id.user_id },
+            userWeight: Number(weight),
+            squat,
+            deadlift,
+            benchpress,
+            SBD_sum: squat + deadlift + benchpress,
+            secondStamp: Number(
+              Math.floor(new Date(faker.date.recent(30).toString()).getTime() / 1000),
+            ),
+            date: dayjs(faker.date.recent(365).toString()).format("YYMMDD"),
+          })
+          .insert()
+          .execute();
+      }),
+    );
+  }
+
+  async mockStatistics() {
+    const allOpenData = await this.getOpenData("sampling");
+    return Promise.all(
+      allOpenData.map((data: OpenDataType) => {
+        return this.statisticsRepository
+          .createQueryBuilder()
+          .insert()
+          .into(SBD_statistics)
+          .values({ gender: data.gender, weight: data.weight, SBD_volume: data.total })
+          .execute();
+      }),
+    );
+  }
+
+  async mockRoutine() {
+    const allUserId = await this.userRepository.createQueryBuilder().select("user_id").getRawMany();
+
+    return Promise.all(
+      allUserId.map(async (id: { user_id: number }) => {
+        return this.routineRepository
+          .createQueryBuilder()
+          .insert()
+          .into(Routine)
+          .values({
+            user: { id: id.user_id },
+            routineName: `${faker.lorem.word()} 루틴`,
+            exerciseName: `${faker.lorem.word()} 운동`,
+            // 기본 3세트
+            exerciseString: `${faker.datatype.number({
+              min: 20,
+              max: 300,
+            })}/${faker.datatype.number({ min: 1, max: 10 })}|${faker.datatype.number({
+              min: 20,
+              max: 300,
+            })}/${faker.datatype.number({ min: 1, max: 10 })}|${faker.datatype.number({
+              min: 20,
+              max: 300,
+            })}/${faker.datatype.number({ min: 1, max: 10 })}`,
+          })
+          .execute();
+      }),
+    );
+  }
+
+  async mockFollow() {
+    const allUserId = await this.userRepository.createQueryBuilder().select("user_id").getRawMany();
+
+    return Promise.all(
+      allUserId.map(async (id: { user_id: number }) => {
+        return this.followRepository
+          .createQueryBuilder()
+          .insert()
+          .into(Follow)
+          .values({})
+          .execute();
+      }),
+    );
+  }
+
+  async mockExercise() {
+    const allUserId = await this.userRepository.createQueryBuilder().select("user_id").getRawMany();
+
+    return Promise.all(
+      allUserId.map(async (id: { user_id: number }) => {
+        return this.exerciseRepository
+          .createQueryBuilder()
+          .insert()
+          .into(Exercise)
+          .values({
+            user: { id: id.user_id },
+            exerciseName: `${faker.lorem.word()} 운동`,
+            // 기본 3세트
+            exerciseString: `${faker.datatype.number({
+              min: 20,
+              max: 300,
+            })}/${faker.datatype.number({ min: 1, max: 10 })}/${faker.datatype.number({
+              min: 0,
+              max: 1,
+            })}|${faker.datatype.number({ min: 20, max: 300 })}/${faker.datatype.number({
+              min: 1,
+              max: 10,
+            })}/${faker.datatype.number({ min: 0, max: 1 })}|${faker.datatype.number({
+              min: 20,
+              max: 300,
+            })}/${faker.datatype.number({ min: 1, max: 10 })}/${faker.datatype.number({
+              min: 0,
+              max: 1,
+            })}`,
+            date: dayjs(faker.date.recent(365).toString()).format("YYMMDD"),
+          })
+          .execute();
+      }),
+    );
+  }
+
+  async mockAlarm() {
+    const allUserId = await this.userRepository.createQueryBuilder().select("user_id").getRawMany();
+    const userIdArray: number[] = [];
+    allUserId.map((id: { user_id: number }) => userIdArray.push(id.user_id));
+
+    return Promise.all(
+      allUserId.map(async (id: { user_id: number }) => {
+        return this.alarmRepository
+          .createQueryBuilder()
+          .insert()
+          .into(Alarm)
+          .values({
+            user: { id: id.user_id },
+            senderUserId: faker.helpers.arrayElement(userIdArray),
+            alarmType: faker.datatype.number({ min: 0, max: 1 }),
+            check: false,
+            minuteStamp: Number(
+              Math.floor(new Date(faker.date.recent(7).toString()).getTime() / 1000 / 60),
+            ),
           })
           .execute();
       }),
