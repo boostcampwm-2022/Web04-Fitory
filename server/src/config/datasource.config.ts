6bf47e7b4a2ba7b9a1f49ebfeb9b4cdd11f92084
edac99 <79911816+edac99@users.noreply.github.com>
2022-11-24 17:03:49 +0900

    🔨 [fix] : tsconfig.json path 적용 오류 수정

--- server/src/config/datasource.config.ts
+++ server/src/config/datasource.config.ts
@@ -1,10 +1,10 @@
-import { Follow } from "./../follows/entities/follow.entity";
-import { SBD_statistics } from "./../sbd_statistics/entities/sbd_statistics.entity";
-import { SBD_record } from "./../sbd_records/entities/sbd_record.entity";
-import { Alarm } from "./../alarms/entities/alram.entity";
-import { Exercise } from "./../exercises/entities/exercise.entity";
-import { Routine } from "./../routines/entities/routine.entity";
-import { User } from "./../users/entities/user.entity";
+import { Follow } from "@follow/entities/follow.entity";
+import { SBD_statistics } from "@statistics/entities/sbd_statistics.entity";
+import { SBD_record } from "@record/entities/sbd_record.entity";
+import { Alarm } from "@alarm/entities/alram.entity";
+import { Exercise } from "@exercise/entities/exercise.entity";
+import { Routine } from "@routine/entities/routine.entity";
+import { User } from "@user/entities/user.entity";
 import { DB_HOST, DB_PORT, DB_USER, DB_PWD, DB_NAME } from "../utils/env";
 import { DataSource } from "typeorm";
 
