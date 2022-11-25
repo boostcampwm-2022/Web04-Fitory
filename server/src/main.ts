9a3b1f7d0682a8533d1b3ecaba70560fea8b96a0
essential2189 <70889358+essential2189@users.noreply.github.com>
2022-11-25 13:30:12 +0900

    🔨 [fix] : cors OPTIONS 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import cookieParser from "cookie-parser";
 import passport from "passport";
-import { Post, ValidationPipe } from "@nestjs/common";
+import { ValidationPipe } from "@nestjs/common";
 import { HttpExceptionFilter } from "@exception/http-exception.filter";
 import { PORT } from "@env";
 import { AppModule } from "./app.module";
@@ -17,7 +17,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: ["http://localhost:8080", "https://www.fitory.ga", "http://localhost:3000"],
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "OPTIONS"],
     credentials: true,
   });
 
