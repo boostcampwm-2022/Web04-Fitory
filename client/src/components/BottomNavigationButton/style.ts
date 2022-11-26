3ef3fe0a73cf93bf7fb15d3ce0faf9896a3f7be9
KingDonggyu <dgkim69166916@gmail.com>
2022-11-26 18:07:13 +0900

    💄 [style] #200 : 하단 네비게이션바 버튼 사이즈 변경

--- client/src/components/BottomNavigationButton/style.ts
+++ client/src/components/BottomNavigationButton/style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const LinkButton = styled(NavLink)`
-  width: 5%;
+  width: 35px;
   filter: ${({ theme }) => theme.COLORS.FILTER_GRAY};
   &.active {
     filter: ${({ theme }) => theme.COLORS.FILTER_BLUE};
