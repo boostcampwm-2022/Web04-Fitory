012d19ff538f7951cf45d7f2d2532d6106725dc0
KingDonggyu <dgkim69166916@gmail.com>
2022-11-28 17:35:15 +0900

    ✨ [feat] #211 : 운동 추가 기능 구현

--- client/src/pages/RecordPage/index.tsx
+++ client/src/pages/RecordPage/index.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import PageTemplate from "@pages/PageTemplate";
 import RoutineScroller from "@components/RoutineScroller";
 import ExersiceInputSet from "@components/ExerciseInputSet";
 import * as s from "./style";
 
 const RecordPage = () => {
+  const [exersiceCount, setExersiceCount] = useState<number>(1);
+  const handleClickExersiceCountButton = () => {
+    setExersiceCount(exersiceCount + 1);
+  };
+
   return (
     <PageTemplate title="운동" isRoot={false}>
       <s.RoutineWrapper>
         <RoutineScroller userName="대구사나이김동규" routineList={["등", "가슴", "어께", "하체"]} />
-        <ExersiceInputSet />
+        {Array.from(Array(exersiceCount)).map((_, i) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <s.ExersiceItem key={i}>
+            <p>#{i + 1}</p>
+            <ExersiceInputSet />
+          </s.ExersiceItem>
+        ))}
+        <s.ExerciseCountButton onClick={handleClickExersiceCountButton}>
+          <span>＋</span>운동 추가
+        </s.ExerciseCountButton>
       </s.RoutineWrapper>
     </PageTemplate>
   );
--- client/src/pages/RecordPage/style.ts
+++ client/src/pages/RecordPage/style.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { DefaultButton } from "@styles/Components";
 
 export const RoutineWrapper = styled.div`
   width: 100%;
@@ -14,4 +15,32 @@ export const RoutineWrapper = styled.div`
 export const RoutineButton = styled.button`
   width: 83px;
   height: 83px;
+`;
+
+export const ExersiceItem = styled.div`
+  margin-bottom: 20px;
+  & > p {
+    margin-bottom: 15px;
+    font-size: ${({ theme }) => theme.FONT_SIZE.SMALL};
+    font-weight: ${({ theme }) => theme.FONT_WEIGHT.BOLD};
+  }
+`;
+
+export const ExerciseCountButton = styled(DefaultButton)`
+  margin-top: 30px;
+  padding: 5px 30px;
+  border-radius: 30px;
+  gap: 10px;
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  background-color: ${({ theme }) => theme.COLORS.DEEP_BLUE};
+  font-size: ${({ theme }) => theme.FONT_SIZE.SMALL};
+  &:hover {
+    background-color: ${({ theme }) => theme.COLORS.DEEP_BLUE};
+    filter: saturate(0.5) !important;
+  }
+  & > span {
+    font-size: ${({ theme }) => theme.FONT_SIZE.EXTRA_LARGE};
+  }
 `;
