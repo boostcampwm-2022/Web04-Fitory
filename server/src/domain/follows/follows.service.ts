e529fe57e9e3adad9c10c2e3d471a9b419b86256
edac99 <jamjam1208@naver.com>
2022-11-28 21:17:35 +0900

    ✨ [feat] #226 : Get Followed List API 구현

--- server/src/domain/follows/follows.service.ts
+++ server/src/domain/follows/follows.service.ts
@@ -1,3 +1,4 @@
+import { User } from "./../users/entities/user.entity";
 import { HttpResponse } from "@converter/response.converter";
 import { Follow } from "@follow/entities/follow.entity";
 import { Injectable } from "@nestjs/common";
@@ -9,13 +10,63 @@ export class FollowsService {
   constructor(
     @InjectRepository(Follow)
     private followRepository: Repository<Follow>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
   ) {}
 
   async getFollowingUserList(userId: number) {
-    return HttpResponse.success({});
+    const followingObject = await this.followRepository
+      .createQueryBuilder("follow")
+      .select("follow.follower_id", "follower_id")
+      .where("follow.followed_id = :userId", { userId })
+      .getRawMany();
+    const followingUserProfileList = await Promise.all(
+      followingObject.map(async (item) => {
+        const following = await this.userRepository
+          .createQueryBuilder("user")
+          .select("user.name", "name")
+          .addSelect("user.profile_image", "profile_image")
+          .addSelect("user.introduce", "introduce")
+          .where("user.id = :userId", { userId: item.follower_id })
+          .getRawOne();
+        return {
+          userId: item.follower_id,
+          userName: following.name,
+          profileImage: following.profile_image,
+          introduce: following.introduce,
+        };
+      }),
+    );
+    return HttpResponse.success({
+      followingUserProfileList,
+    });
   }
 
   async getFollowerUserList(userId: number) {
-    return HttpResponse.success({});
+    const followerObject = await this.followRepository
+      .createQueryBuilder("follow")
+      .select("follow.followed_id", "followed_id")
+      .where("follow.follower_id = :userId", { userId })
+      .getRawMany();
+    const followerUserProfileList = await Promise.all(
+      followerObject.map(async (item) => {
+        const following = await this.userRepository
+          .createQueryBuilder("user")
+          .select("user.name", "name")
+          .addSelect("user.profile_image", "profile_image")
+          .addSelect("user.introduce", "introduce")
+          .where("user.id = :userId", { userId: item.followed_id })
+          .getRawOne();
+        return {
+          userId: item.followed_id,
+          userName: following.name,
+          profileImage: following.profile_image,
+          introduce: following.introduce,
+        };
+      }),
+    );
+    return HttpResponse.success({
+      followerUserProfileList,
+    });
   }
 }
