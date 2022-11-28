163e1e0fb41263a1daee9756c1b599703b0568ac
KingDonggyu <dgkim69166916@gmail.com>
2022-11-28 14:34:00 +0900

    ✨ [feat] #211 : 운동 기록 페이지 라우팅 설정

---
+++ client/src/pages/RecordPage/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageTemplate from "@pages/PageTemplate";
+
+const RecordPage = () => {
+  return (
+    <PageTemplate isRoot={false}>
+      <div>record</div>
+    </PageTemplate>
+  );
+};
+
+export default RecordPage;
