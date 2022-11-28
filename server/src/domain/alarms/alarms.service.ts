8f0058922e5562ab94714f3b36a280c569ad340f
edac99 <jamjam1208@naver.com>
2022-11-28 16:50:12 +0900

    ✨ [feat] : Get Alarm List API 구현
    특정 사용자가 받은 알람들의 정보를 응답

--- server/src/domain/alarms/alarms.service.ts
+++ server/src/domain/alarms/alarms.service.ts
@@ -3,12 +3,15 @@ import { Alarm } from "./entities/alram.entity";
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { User } from "@user/entities/user.entity";
 
 @Injectable()
 export class AlarmsService {
   constructor(
     @InjectRepository(Alarm)
     private alarmRepository: Repository<Alarm>,
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
   ) {}
 
   async countUnreadAlarm(userId: number) {
@@ -22,6 +25,31 @@ export class AlarmsService {
   }
 
   async getAlarmList(userId: number) {
-    return HttpResponse.success({});
+    const senderUserIdObject = await this.alarmRepository
+      .createQueryBuilder("alarm")
+      .select("alarm.sender_user_id", "sender_user_id")
+      .addSelect("alarm.alarm_type", "alarm_type")
+      .addSelect("alarm.time_stamp", "time_stamp")
+      .addSelect("alarm.check", "check")
+      .where("alarm.user_id = :userId", { userId })
+      .getRawMany();
+    const result = await Promise.all(
+      senderUserIdObject.map(async (item) => {
+        const userInfo = await this.usersRepository
+          .createQueryBuilder("user")
+          .select("user.name", "name")
+          .addSelect("user.profile_image", "profile_image")
+          .where("user.user_id = :senderUserId", { senderUserId: item.sender_user_id })
+          .getRawOne();
+        return {
+          ...item,
+          senderName: userInfo.name,
+          senderProfileImage: userInfo.profile_image,
+        };
+      }),
+    );
+    return HttpResponse.success({
+      result,
+    });
   }
 }
