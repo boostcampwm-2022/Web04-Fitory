998a5ac31ae3afb5faaf54b839cae74dc6e7bd75
KingDonggyu <dgkim69166916@gmail.com>
2022-11-29 19:49:58 +0900

    ✨ [feat] #230 : 구글 로그인 API 라이브러리 변경 및 적용

--- client/src/components/GoogleOAuthButton/index.tsx
+++ client/src/components/GoogleOAuthButton/index.tsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { gapi } from "gapi-script";
+import { GoogleLogin, GoogleLoginResponse } from "react-google-login";
 import useGoogleLogin from "@hooks/query/useGoogleLogin";
-import { GoogleLogin, CredentialResponse } from "@react-oauth/google";
+import googleSignInSrc from "@public/images/btn_google_signin.png";
+import * as s from "./style";
 
 const GoogleOAuthButton = () => {
+  const googleClientId = process.env.GOOGLE_CLIENT_ID as string;
   const { googleLogin } = useGoogleLogin();
 
-  const handleSuccessLogin = ({ credential }: CredentialResponse) => {
-    if (credential) {
-      googleLogin({ accessToken: credential });
-    }
+  const handleSuccessLogin = ({ accessToken }: GoogleLoginResponse) => {
+    googleLogin({ accessToken });
   };
 
-  return <GoogleLogin onSuccess={handleSuccessLogin} useOneTap />;
+  useEffect(() => {
+    gapi.load("client:googleOAuth", () => {
+      gapi.client.init({ clientId: googleClientId, scope: "" });
+    });
+  });
+
+  return (
+    <GoogleLogin
+      clientId={googleClientId}
+      render={(renderProps) => (
+        <s.Button onClick={renderProps.onClick} disabled={renderProps.disabled}>
+          <img src={googleSignInSrc} alt="구글 로그인 버튼" />
+        </s.Button>
+      )}
+      onSuccess={(response) => handleSuccessLogin(response as GoogleLoginResponse)}
+    />
+  );
 };
 
 export default GoogleOAuthButton;
