b5a29f6e251ad3a69d19713e8471142d9e523abe
KingDonggyu <dgkim69166916@gmail.com>
2022-11-30 19:20:21 +0900

    ♻️ [refactor] #230 : 쿠키 핸들 모듈 제거

--- client/src/pages/ProfilePage/index.tsx
+++ client/src/pages/ProfilePage/index.tsx
@@ -2,15 +2,12 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import PageTemplate from "@pages/PageTemplate";
 import { RoutePath } from "@constants/enums";
-import { authCookie } from "src/services/Cookie";
 
 const ProfilePage = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!authCookie.get()) {
-      navigate(RoutePath.LOGIN, { replace: true });
-    }
+    navigate(RoutePath.LOGIN, { replace: true });
   }, [navigate]);
 
   return (
