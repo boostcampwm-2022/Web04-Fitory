d3a79c55aabedf2bfc499a707a112ba10749cbe6
essential2189 <70889358+essential2189@users.noreply.github.com>
2022-11-30 22:45:48 +0900

    ✨ [feat] #251 : jwt strategy

---
+++ server/src/validate/jwt/jwt.strategy.ts
@@ -0,0 +1,41 @@
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { PassportStrategy } from "@nestjs/passport";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Request } from "express";
+import { User } from "@user/entities/user.entity";
+import { ACCESS_TOKEN_SECRETKEY } from "@env";
+import { JwtPayload } from "@type/jwt";
+import { Exception } from "@exception/exceptions";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(@InjectRepository(User) private userRepository: Repository<User>) {
+    const extractJwtFromCookie = (req: Request) => {
+      let token = null;
+      if (req && req.cookies) {
+        token = req.cookies.access_token;
+      }
+      return token || ExtractJwt.fromAuthHeaderAsBearerToken()(req);
+    };
+
+    super({
+      ignoreExpiration: false,
+      secretOrKey: ACCESS_TOKEN_SECRETKEY,
+      jwtFromRequest: extractJwtFromCookie,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const userId = await this.userRepository
+      .createQueryBuilder("user")
+      .where("user.userId = :userId", { userId: payload.userId })
+      .select("user.userId")
+      .getOne();
+
+    if (!userId) throw new Exception().Unauthorized();
+
+    return userId;
+  }
+}
