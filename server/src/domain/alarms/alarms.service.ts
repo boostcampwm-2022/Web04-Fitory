af666b3d3fa429ab6780fd7c0195669cddab7e35
edac99 <jamjam1208@naver.com>
2022-12-01 22:47:29 +0900

    ✨ [feat] #284 : 1달 이내의 알림들만 보여주도록 수정
    알림 페이지 들어가면 안읽은 알람 + 읽었지만 온 지 1달이 아직 안지난 알람
    이렇게 보여주도록 수정

--- server/src/domain/alarms/alarms.service.ts
+++ server/src/domain/alarms/alarms.service.ts
@@ -36,7 +36,7 @@ export class AlarmsService {
       .addSelect("alarm.time_stamp", "time_stamp")
       .innerJoin(User, "user", "user.user_id = alarm.sender_user_id")
       .where("alarm.user_id = :userId", { userId })
-      .andWhere("alarm.check = false")
+      .andWhere("DATE(alarm.time_stamp) >= DATE_ADD(NOW(), INTERVAL -1 MONTH)")
       .getRawMany();
     await this.checkToReadAlarm(userId);
     return HttpResponse.success({
