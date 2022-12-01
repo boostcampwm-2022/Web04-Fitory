5290b2fcdd9b6e227152109506e06f72d13bf9b0
essential2189 <70889358+essential2189@users.noreply.github.com>
2022-12-01 14:38:00 +0900

    ♻️ [refactor] #251 : 리팩토링

--- server/src/validate/validate.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-import { User } from "@user/entities/user.entity";
-
-export const GetUserId = createParamDecorator((data, ctx: ExecutionContext): User => {
-  const req = ctx.switchToHttp().getRequest();
-
-  return req.userId;
-});
