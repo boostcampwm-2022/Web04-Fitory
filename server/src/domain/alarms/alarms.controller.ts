ffa5b9dd72cb68aa8891b73f62ba0431261eb49b
edac99 <jamjam1208@naver.com>
2022-12-05 15:04:25 +0900

    ✨ [feat] #328 : SSE로 읽지 않은 알림 응답
    1초마다 프론트로 SSE 테스트 완료

--- server/src/domain/alarms/alarms.controller.ts
+++ server/src/domain/alarms/alarms.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Query } from "@nestjs/common";
-import { ApiOperation, ApiQuery, ApiTags } from "@nestjs/swagger";
+import { Controller, Get, Param, Query, Sse } from "@nestjs/common";
+import { ApiOperation, ApiParam, ApiQuery, ApiTags } from "@nestjs/swagger";
 import { Exception } from "@exception/exceptions";
 import { isValidUserId } from "@validation/validation";
 import { AlarmsService } from "./alarms.service";
 import { UsersService } from "@user/users.service";
+import { interval, map, Observable, switchMap } from "rxjs";
 
 @Controller("api/alarms")
 @ApiTags("ALARM API")
@@ -24,6 +25,26 @@ export class AlarmsController {
   async getUnreadAlarmCount(@Query("userId") userId: number) {
     if (!isValidUserId(userId)) throw new Exception().invalidUserIdError();
     return this.alarmService.countUnreadAlarm(userId);
+  }
+
+  // sse
+  @Sse("unread")
+  @ApiOperation({
+    summary: "해당 사용자가 읽지 않은 알림 수를 반환",
+  })
+  @ApiQuery({
+    name: "userId",
+    type: "number",
+  })
+  async sse(@Query("userId") userId: number): Promise<Observable<MessageEvent>> {
+    return interval(1000).pipe(
+      switchMap(async () => await this.alarmService.countUnreadAlarm(userId)),
+      map((prop) => {
+        return {
+          data: { unreadAlarmCount: prop.response.alarmCount },
+        } as MessageEvent;
+      }),
+    );
   }
 
   @Get("list")
