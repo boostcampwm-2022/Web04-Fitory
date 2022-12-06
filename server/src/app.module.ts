63804e5900222fa5cb55498929bd15b4a690be9f
essential2189 <70889358+essential2189@users.noreply.github.com>
2022-12-06 17:32:11 +0900

    🔨 [fix] #346 : guard 버그 수정

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -15,7 +15,6 @@ import { MockModule } from "@mock/mock.module";
 import { JwtStrategy } from "@guard/jwt.strategy";
 import { JwtAuthGuard } from "@guard/jwt.guard";
 import { User } from "@user/entities/user.entity";
-import { UserIdGuard } from "@guard/auth.guard";
 import { UploadModule } from "./domain/uploads/upload.module";
 import { typeormConfig } from "./config/typeorm.config";
 
@@ -41,10 +40,6 @@ import { typeormConfig } from "./config/typeorm.config";
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: UserIdGuard,
     },
     { provide: APP_FILTER, useClass: HttpExceptionFilter },
   ],
--- server/src/guard/jwt.guard.ts
+++ server/src/guard/jwt.guard.ts
@@ -1,5 +1,26 @@
-import { Injectable } from "@nestjs/common";
+import { ExecutionContext, Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+import { Exception } from "@exception/exceptions";
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard("jwt") {}
+export class JwtAuthGuard extends AuthGuard("jwt") {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const noAuth = this.reflector.get<boolean>("no-auth", context.getHandler());
+
+    if (noAuth) return true;
+
+    const req = context.switchToHttp().getRequest();
+
+    if (Number(req.headers.user_id) !== req.user) {
+      throw new Exception().guardError();
+    }
+
+    return true;
+  }
+}
