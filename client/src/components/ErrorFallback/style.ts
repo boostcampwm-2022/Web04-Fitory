cfb6715a3c80b51fe81f5dcd9ea78feab54bf2e2
KingDonggyu <dgkim69166916@gmail.com>
2022-12-07 17:59:54 +0900

    ✨ [feat] #362 : Error Boundary 구현

---
+++ client/src/components/ErrorFallback/style.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  gap: 20px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: ${({ theme }) => theme.MAX_WIDTH.DEFAULT};
+`;
+
+export const Logo = styled.img`
+  margin-bottom: 50px;
+  width: 200px;
+`;
+
+export const Title = styled.h1`
+  gap: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: ${({ theme }) => theme.FONT_SIZE.EXTRA_LARGE};
+  font-weight: ${({ theme }) => theme.FONT_WEIGHT.BOLD};
+  @media screen and (max-width: ${({ theme }) => theme.MAX_WIDTH.MOBILE}) {
+    font-size: ${({ theme }) => theme.FONT_SIZE.MEDIUM};
+  }
+  & > span {
+    color: ${({ theme }) => theme.COLORS.LIGHT_BLUE};
+    font-size: ${({ theme }) => theme.FONT_SIZE.EXTRA_LARGE};
+  }
+`;
+
+export const SorryText = styled.h2`
+  margin-top: 50px;
+  font-size: ${({ theme }) => theme.FONT_SIZE.EXTRA_LARGE};
+  font-weight: ${({ theme }) => theme.FONT_WEIGHT.BOLD};
+  color: ${({ theme }) => theme.COLORS.DEEP_GRAY};
+`;
