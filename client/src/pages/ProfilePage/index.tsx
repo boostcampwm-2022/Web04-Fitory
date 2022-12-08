92fa7693a8169a887622e511274d9747f61ef5ff
Choi siun <siun0331@gmail.com>
2022-12-08 20:32:46 +0900

    ♻️ [refactor]#382: 마이페이지 소개 컴포넌트 관심사에 맞게 합침

--- client/src/pages/ProfilePage/index.tsx
+++ client/src/pages/ProfilePage/index.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import PageTemplate from "@pages/PageTemplate";
 import MyPageUserProfile from "@components/MyPageUserProfile";
-import MyPageUserIntroduce from "@components/MyPageUserIntroduce";
-import MyPageSubInfoContainer from "@components/MyPageSubInfoContainer";
 import MyPageEditButton from "@components/MyPageEditButton";
 import useUserInfo from "@hooks/query/useUserInfo";
 import FollowButton from "@components/FollowButton";
@@ -21,8 +19,6 @@ const ProfilePage = () => {
     <PageTemplate isRoot={isOwner}>
       <s.MyProfileContainer>
         <MyPageUserProfile userId={profileUserId} isOwner={isOwner} />
-        <MyPageUserIntroduce userId={profileUserId} isOwner={isOwner} />
-        <MyPageSubInfoContainer userId={profileUserId} />
         <s.ButtonContainer>
           {isOwner ? (
             <MyPageEditButton userId={profileUserId} ownerId={id} isOwner={isOwner} />
