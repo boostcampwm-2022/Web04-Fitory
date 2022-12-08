282b646c8c3eb479602147483e21f8410e80a35e
Choi siun <siun0331@gmail.com>
2022-12-08 18:32:05 +0900

    ♻️ [refactor]#384: 팔로우 버튼 클릭시 즉시 상태변경 후 리렌더링

--- client/src/components/FollowButton/index.tsx
+++ client/src/components/FollowButton/index.tsx
@@ -6,24 +6,26 @@ import { FollowUserInfo } from "../../types/user";
 
 const FollowButton = ({ otherUserId, myUserId }: FollowUserInfo) => {
   const [followState, setFollowState] = useState(true);
-  // false == 팔로우중 true: 팔로우 가능
-  const handleButtonClick = async () => {
-    if (followState) {
-      return FollowAPI.follow({ myUserId, otherUserId });
-    }
-    return FollowAPI.unFollow({ myUserId, otherUserId });
+  const handleButtonClick = () => {
+    (async () => {
+      if (followState) {
+        return FollowAPI.follow({ myUserId, otherUserId });
+      }
+      return FollowAPI.unFollow({ myUserId, otherUserId });
+    })();
+    return setFollowState(!followState);
   };
   useEffect(() => {
     (async () => {
       const ownerFollowList = await UserAPI.getFollowingUser(myUserId);
       ownerFollowList.map((user) => {
-        if (user.follower_id === otherUserId) {
+        if ((user.follower_id || user.followed_id) === otherUserId) {
           return setFollowState(false);
         }
         return true;
       });
     })();
-  }, [followState]);
+  }, []);
   return (
     <s.ProfileButton followState={followState} type="button" onClick={handleButtonClick}>
       {followState ? "팔로우" : "팔로우 취소"}
