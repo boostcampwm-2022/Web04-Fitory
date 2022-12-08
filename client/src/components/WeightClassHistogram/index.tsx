ca9106f89cbe89dd2164e331d0023d645dd5d50a
KingDonggyu <dgkim69166916@gmail.com>
2022-12-08 17:15:43 +0900

    ✨ [feat] #343 : 히스토그램 내 사용자가 속하는 bar 표시

--- client/src/components/WeightClassHistogram/index.tsx
+++ client/src/components/WeightClassHistogram/index.tsx
@@ -1,23 +1,32 @@
 import React from "react";
-import { ChartData, ChartOptions } from "chart.js";
+import { ChartData, ChartOptions, ScriptableContext, ChartTypeRegistry } from "chart.js";
 import { Bar } from "react-chartjs-2";
+import styled from "styled-components";
 import useUserInfo from "@hooks/query/useUserInfo";
+import useBestChallengeScore from "@hooks/query/useBestChallengeScore";
 import useWeightClassStatistics from "@hooks/query/useWeightClassStatistics";
 import { WEIGHT_CLASS_STATISTICS_RANGE } from "@constants/consts";
 import { authStorage } from "src/services/ClientStorage";
 
-const options: ChartOptions = {
-  maintainAspectRatio: false,
-  plugins: {
-    title: {
-      display: true,
-      text: "3대 챌린지 중량별 인원 분포",
-    },
-  },
+const XLabel = styled.p`
+  margin-top: 15px;
+`;
+
+const checkMyBarItem = (ctx: ScriptableContext<keyof ChartTypeRegistry>, myBestScore: number) => {
+  const { labels } = ctx.chart.data;
+
+  if (!labels || labels.length - 1 < ctx.dataIndex) {
+    return false;
+  }
+
+  const score = +(labels[ctx.dataIndex] as string);
+  return score <= myBestScore && myBestScore < score + WEIGHT_CLASS_STATISTICS_RANGE;
 };
 
 const WeightClassHistogram = () => {
   const { userInfo } = useUserInfo(authStorage.get());
+  const { bestChallengeScore } = useBestChallengeScore();
+
   const { weightClassStatistics } = useWeightClassStatistics({
     range: WEIGHT_CLASS_STATISTICS_RANGE,
     weight: userInfo.weight,
@@ -30,18 +39,44 @@ const WeightClassHistogram = () => {
       {
         type: "bar",
         label: "인원 수",
-        backgroundColor: "#ffb3c2",
         data: weightClassStatistics.responseData.map(({ y }) => y),
-        borderColor: "#ff6384",
         borderWidth: 2,
+      },
+      {
+        type: "bar",
+        label: "내 소속",
+        data: [],
+        borderWidth: 2,
+        borderColor: "#37a2eb",
+        backgroundColor: "#a0d0f6",
       },
     ],
   };
 
+  const options: ChartOptions<"bar"> = {
+    maintainAspectRatio: false,
+    elements: {
+      bar: {
+        backgroundColor: (ctx) =>
+          checkMyBarItem(ctx, bestChallengeScore?.SBD_sum || 0) ? "#a0d0f6" : "#ffb3c2",
+        borderColor: (ctx) =>
+          checkMyBarItem(ctx, bestChallengeScore?.SBD_sum || 0) ? "#37a2eb" : "#ff6384",
+      },
+    },
+    scales: {
+      x: {
+        stacked: true,
+      },
+    },
+  };
+
   return (
-    <Bar options={options} data={data}>
-      test
-    </Bar>
+    <>
+      <Bar options={options} data={data}>
+        test
+      </Bar>
+      <XLabel>3대 챌린지 중량 합 (kg)</XLabel>
+    </>
   );
 };
 
