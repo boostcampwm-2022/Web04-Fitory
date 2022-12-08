e895ec8e10d8d3c1e29b91bef402c1554834b294
KingDonggyu <dgkim69166916@gmail.com>
2022-12-09 02:49:52 +0900

    💄 [style] #394 : 잔디 & 루틴 리스트 배치

--- client/src/pages/ProfilePage/style.ts
+++ client/src/pages/ProfilePage/style.ts
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
-export const MyProfileContainer = styled.div`
-  background-color: ${({ theme }) => theme.COLORS.WHITE};
-  padding: 30px 80px;
+const Wrapper = styled.div`
   height: fit-content;
+
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+`;
+
+export const MyProfileContainer = styled(Wrapper)`
+  padding: 30px 80px;
   @media screen and (max-width: ${({ theme }) => theme.MAX_WIDTH.MOBILE}) {
     padding: 20px;
   }
@@ -12,4 +16,23 @@ export const MyProfileContainer = styled.div`
 export const ButtonContainer = styled.div`
   height: 50px;
   font-size: ${({ theme }) => theme.FONT_SIZE.SMALL};
+`;
+
+export const BottomWrapper = styled(Wrapper)`
+  margin-top: 10px;
+  padding-top: 30px;
+`;
+
+export const BottomTitle = styled.h2`
+  font-size: ${({ theme }) => theme.FONT_SIZE.MEDIUM};
+  font-weight: ${({ theme }) => theme.FONT_WEIGHT.BOLD};
+`;
+
+export const ZandiLabel = styled.p`
+  padding-left: 20px;
+  color: ${({ theme }) => theme.COLORS.DEEP_GRAY};
+  font-size: ${({ theme }) => theme.FONT_SIZE.EXTRA_SMALL};
+  & > span {
+    font-weight: ${({ theme }) => theme.FONT_WEIGHT.BOLD};
+  }
 `;
