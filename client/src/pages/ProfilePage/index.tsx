2df5a7ed0772b049edfa9e5c1a992d9e9a976019
Choi siun <siun0331@gmail.com>
2022-12-09 01:57:15 +0900

    ✨ [feat]#400: 팔로우페이지에서 뒤로가기로 마이페이지 이동시, 실시간 정보가 반영 안되던 현상 해결

--- client/src/pages/ProfilePage/index.tsx
+++ client/src/pages/ProfilePage/index.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import PageTemplate from "@pages/PageTemplate";
 import MyPageUserProfile from "@components/MyPageUserProfile";
 import MyPageEditButton from "@components/MyPageEditButton";
 import useUserInfo from "@hooks/query/useUserInfo";
 import FollowButton from "@components/FollowButton";
-import * as s from "./style";
+import { QueryKey } from "@constants/enums";
+import { useQueryClient } from "react-query";
+import UserAPI from "@api/UserAPI";
 import { authStorage } from "../../services/ClientStorage";
+import * as s from "./style";
 
 const ProfilePage = () => {
   const { userId } = useParams();
@@ -14,7 +17,13 @@ const ProfilePage = () => {
   const { userInfo } = useUserInfo(profileUserId);
   const { id } = userInfo;
   const isOwner = profileUserId === authStorage.get();
+  const queryClient = useQueryClient();
 
+  useEffect(() => {
+    (() => {
+      return queryClient.invalidateQueries([QueryKey.USER_INFO, id]);
+    })();
+  }, []);
   return (
     <PageTemplate isRoot={isOwner}>
       <s.MyProfileContainer>
