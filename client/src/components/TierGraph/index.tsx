f6ff01d00aefaea55e044ff540cfe4fc3f181e8d
Choi siun <siun0331@gmail.com>
2022-12-09 03:16:44 +0900

    ✨ [feat]#316: 통계 데이터 기반 화면에 출력 및 데이터 없을 시 대체 텍스트 출력

--- client/src/components/TierGraph/index.tsx
+++ client/src/components/TierGraph/index.tsx
@@ -3,22 +3,27 @@ import { Chart, registerables, Plugin } from "chart.js";
 import { Line } from "react-chartjs-2";
 import { judColors } from "@components/TierGraph/utils";
 import { AnyObject } from "immer/dist/types/types-internal";
+import ExerciseAPI from "@api/ExerciseAPI";
 import options from "./option";
 import plugin from "./plugin";
-import mockData from "./mockData";
+import { SDBRecordHistory, SDBRecordHistoryArray } from "../../types/exercise";
+import * as s from "./style";
 
 Chart.register(...registerables);
-
-interface MockDataType {
-  date: string;
-  score: number;
-}
 
 const backgroundColorArray: string[] = [];
 
 const TierGraph = () => {
   const [labelInfo, setLabelInfo] = useState<string[]>([]);
   const [dataInfo, setDataInfo] = useState<number[]>([]);
+  const [userExerciseHistory, setUserExerciseHistory] = useState<SDBRecordHistoryArray[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const historyList = await ExerciseAPI.getEveryDayHistory();
+      setUserExerciseHistory(historyList);
+    })();
+  }, []);
 
   const graphData = {
     labels: labelInfo,
@@ -39,17 +44,21 @@ const TierGraph = () => {
   useEffect(() => {
     const dates: string[] = [];
     const scores: number[] = [];
-    mockData.forEach((item: MockDataType) => {
-      dates.push(item.date);
-      scores.push(item.score);
-      backgroundColorArray.push(judColors(item.score));
+    userExerciseHistory.forEach((item: SDBRecordHistoryArray) => {
+      const { record } = item;
+      const date = new Date(record.timeStamp).toISOString().substring(0, 10).replace(/-/g, "");
+      dates.push(date);
+      scores.push(record.SBD_sum);
+      backgroundColorArray.push(judColors(record.SBD_sum));
     });
     setLabelInfo(dates);
     setDataInfo(scores);
-  }, []);
+  }, [userExerciseHistory]);
 
-  return (
+  return userExerciseHistory.length ? (
     <Line data={graphData} plugins={plugin as Plugin<"line", AnyObject>[]} options={options} />
+  ) : (
+    <s.DefaultContainer> 표시할 티어 정보가 없습니다. </s.DefaultContainer>
   );
 };
 
