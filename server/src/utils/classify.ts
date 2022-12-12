392006d786efbdfd8fb0ae3dc59bff446287f63d
edac99 <jamjam1208@naver.com>
2022-12-12 13:44:42 +0900

    ✨ [feat] #413: 체급별 티어 분류 기준 추가
    DB에 의도대로 반영됨을 확인

--- server/src/utils/classify.ts
+++ server/src/utils/classify.ts
@@ -1,17 +1,23 @@
+export const weightGradeList = [53, 59, 66, 74, 83, 93, 105, 120];
+
+export const volumeGradeList = [
+  [110, 190, 223, 313, 404],
+  [127, 213, 254, 359, 462],
+  [142, 241, 287, 396, 505],
+  [159, 266, 318, 437, 558],
+  [175, 289, 344, 475, 605],
+  [186, 309, 371, 504, 638],
+  [198, 331, 398, 541, 679],
+  [200, 335, 400, 542, 681],
+  [212, 352, 422, 572, 715],
+];
+
 export function classifyToTier(weight: number, volume: number) {
-  let tier = 0;
-  if (0 <= volume && volume < 100) {
-    tier = 1;
-  } else if (100 <= volume && volume < 200) {
-    tier = 2;
-  } else if (200 <= volume && volume < 300) {
-    tier = 3;
-  } else if (300 <= volume && volume < 400) {
-    tier = 4;
-  } else if (400 <= volume && volume < 500) {
-    tier = 5;
-  } else if (500 <= volume && volume < 600) {
-    tier = 6;
-  }
+  const weightGrade = weightGradeList.reduce((acc, cur, index) => {
+    return weight > cur ? index + 1 : acc;
+  }, 0);
+  const tier = volumeGradeList[weightGrade].reduce((acc, cur, index) => {
+    return volume > cur ? index + 1 : acc;
+  }, 0);
   return tier;
 }
