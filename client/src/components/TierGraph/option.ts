f8e646ce5cfbc3748117f6d3f7b23716d5fc886f
Choi siun <siun0331@gmail.com>
2022-12-12 18:38:02 +0900

    ✨ [feat]#316: 통계페이지 티어 그래프 기준 값 반영

--- client/src/components/TierGraph/option.ts
+++ client/src/components/TierGraph/option.ts
@@ -1,58 +1,74 @@
 import theme from "@styles/Theme";
+import { judLine, returnTierStandard } from "@components/TierGraph/utils";
 
-const options = {
-  plugins: {
-    legend: {
-      display: false,
-    },
-  },
-  intersect: false,
-  lineTension: 0,
-  pointRadius: 3,
-  pointHoverRadius: 5,
-  maintainAspectRatio: false,
-
-  scales: {
-    x: {
-      grid: {
-        display: false,
-      },
-    },
-    y: {
-      grid: {
-        display: false,
-      },
-      min: 0,
-      max: 700,
-    },
-  },
-  horizontalLine: [
+const setHorizontalLineLine = (weight: number) => {
+  const standard = returnTierStandard(weight);
+  return [
     {
-      y: 200,
+      y: standard[1],
       style: theme.TIER_COLOR.SILVER,
       text: "S",
     },
     {
-      y: 300,
+      y: standard[2],
       style: theme.TIER_COLOR.GOLD,
       text: "G",
     },
     {
-      y: 400,
+      y: standard[3],
       style: theme.TIER_COLOR.PLATINUM,
       text: "P",
     },
     {
-      y: 500,
+      y: standard[4],
       style: theme.TIER_COLOR.DIAMOND,
       text: "D",
     },
     {
-      y: 600,
+      y: standard[5],
       style: theme.TIER_COLOR.CHAMPION,
       text: "C",
     },
-  ],
+  ];
+};
+const drawChartOption = (scores: number[], weight: number) => {
+  const maxScore = scores.length !== 0 ? Math.max(...scores) : 1000;
+  const minScore = scores.length !== 0 ? Math.min(...scores) : 0;
+
+  const minLine = judLine(minScore, weight);
+  const maxLine = judLine(maxScore, weight);
+
+  const line = setHorizontalLineLine(weight).slice(minLine - 1, maxLine + 2);
+
+  return {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    intersect: false,
+    lineTension: 0,
+    pointRadius: 3,
+    pointHoverRadius: 5,
+    maintainAspectRatio: false,
+
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+        min: minScore - 50,
+        max: maxScore + 100,
+      },
+    },
+
+    horizontalLine: line,
+  };
 };
 
-export default options;
+export default drawChartOption;
