7ea5cc8d0ae9d0410c7c9b91c1b5b9ba1d5623d6
Choi siun <siun0331@gmail.com>
2022-12-14 06:38:24 +0900

    🔨 [fix]#496: 프로필 사진 반영 안되는 버그 해결

--- client/src/components/SearchedUserList/index.tsx
+++ client/src/components/SearchedUserList/index.tsx
@@ -6,9 +6,10 @@ import { SearchedUserInfo } from "../../types/user";
 const SearchedUserList = ({ searchedUser }: { searchedUser: SearchedUserInfo[] }) => {
   return (
     <>
-      {searchedUser.map(({ user_id, follower_id, followed_id, name, introduce }) => (
+      {searchedUser.map(({ user_id, follower_id, followed_id, name, introduce, profile_image }) => (
         <s.UserProfile key={follower_id || followed_id || user_id}>
           <SearchResultUserProfile
+            profileImgUrl={profile_image}
             userName={name}
             userMessage={introduce}
             profileId={(follower_id || followed_id || user_id) as number}
