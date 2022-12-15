65001c564cff95e597404a8b5bf03b4e4eb6cce2
KingDonggyu <dgkim69166916@gmail.com>
2022-12-15 16:34:25 +0900

    💄 [style] : 검색 페이지 내 추천 사용자 리스트 스타일 수정

--- client/src/components/RecommendUserCard/style.ts
+++ client/src/components/RecommendUserCard/style.ts
@@ -1,25 +1,26 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  width: 150px;
-  height: 200px;
+  width: 130px;
   padding: 10px;
+  flex-direction: column;
+  align-items: center;
+  display: flex;
   text-align: center;
 `;
 
 export const UserInfoContainer = styled.div`
   width: 130px;
-  height: 145px;
 `;
 
 export const ProfileImageContainerWrapper = styled.div`
-  width: 90px;
-  height: 90px;
+  width: 80px;
+  height: 80px;
   margin: auto;
 `;
 
 export const UserNameLabel = styled.div`
-  margin: 20px 0;
+  margin: 15px 0;
   cursor: pointer;
   color: ${({ theme }) => theme.COLORS.DEEP_BLUE};
   font-size: ${({ theme }) => theme.FONT_SIZE.EXTRA_SMALL};
